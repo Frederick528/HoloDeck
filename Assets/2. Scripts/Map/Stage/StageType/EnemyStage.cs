398276a6dde4e891cfc9a2c720939a0e579756c2
Lei da sorte <75717568+Frederick528@users.noreply.github.com>
2024-09-07 00:26:35 +0900

    Update EnemyStage.cs

--- Assets/2. Scripts/Map/Stage/StageType/EnemyStage.cs
+++ Assets/2. Scripts/Map/Stage/StageType/EnemyStage.cs
@@ -13,10 +13,42 @@ public class EnemyStage : MonoBehaviour, IStage
         if (!map.cleared)
         {
             TurnManager.Instance.StartBattle();
-            EnemyManager.Instance.SpawnEnemy(100, 0);
-            EnemyManager.Instance.SpawnEnemy(101, 1);
+            EnemySpawn(0);
         }
             print("Enemy");
+    }
+
+    public void EnemySpawn(int mapLV)
+    {
+        switch (mapLV)
+        {
+            case 0:
+                EnemySpawnPattern(Random.Range(0, 2));
+                break;
+            case 1:
+                EnemySpawnPattern(Random.Range(100, 110));
+                break;
+            case 2:
+                EnemySpawnPattern(Random.Range(200, 210));
+                break;
+        }
+        
+    }
+
+    public void EnemySpawnPattern(int rand)
+    {
+        switch (rand)
+        {
+            case 0:
+                EnemyManager.Instance.SpawnEnemy(100, 0);
+                EnemyManager.Instance.SpawnEnemy(101, 1);
+                break;
+            case 1:
+                EnemyManager.Instance.SpawnEnemy(100, 0);
+                EnemyManager.Instance.SpawnEnemy(100, 1);
+                EnemyManager.Instance.SpawnEnemy(100, 2);
+                break;
+        }
     }
 
 }
