d308442e1555ec55197d305b6483adfb51792d53
Frederick528 <75717568+Frederick528@users.noreply.github.com>
2025-03-05 18:29:51 +0900

    사실 오류는 아니고, 이름 변경한 것 때문에 다른 컴에서 켰을 때, 인식이 안 되는 버그.

---
+++ Assets/2. Scripts/Manager/UIManager.cs
@@ -0,0 +1,377 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    public enum CanvasName
+    {
+        InGame,
+        GameOver,
+        Battle,
+        Map,
+        RewardBox,
+        CardReward,
+        ItemReward,
+        Shop,
+        SelectedCard,
+        ViewDeck
+    }
+    //public List<GameObject> CanvasList;
+    List<GraphicRaycaster> _canvasRaycaster = new();
+    public Dictionary<int, Transform> CanvasDict = new();
+
+    Transform _canvas;
+    [HideInInspector]
+    public Transform Player;
+
+
+
+    [Header("UICardPrefab")]
+    [SerializeField] UICard _uiCard;
+
+    [Header("Panel")]
+    Transform _cardEnlargePanel;
+    Transform _itemEnlargePanel;
+
+
+    Transform _shopPanel;
+    Transform _shopEnlargePanel;
+
+    [Header("Box")]
+    Transform[] _rewardBoxes;
+
+    [HideInInspector]
+    public Transform PassiveTransform;
+    [HideInInspector]
+    public Transform ActiveTransform;
+    [HideInInspector]
+    public Transform PotionTransform;
+
+    Transform _cardRewardContent;
+    Transform _itemRewardContent;
+
+    int _lastViewDeckCount;
+    Transform _viewDeckContent;
+    List<UICard> _deckUICards = new();
+
+    UICard[] _uiCards = new UICard[4];
+    UIItem[] _uiItems = new UIItem[4];
+
+    TMP_Text _topHealthText;     // TMP텍스트로 변경가능성있음
+    TMP_Text _topCoinText;
+
+    TMP_Text _holoValue;
+    TMP_Text _turnEndButtonText;
+    TMP_Text _drawCount;
+    TMP_Text _dummyCount;
+
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _canvas = GameObject.Find("Canvas").GetComponent<Transform>();
+        Player = GameObject.Find("Player").GetComponent<Transform>();
+
+        for (int i = 0; i < /*CanvasList.Count*/_canvas.childCount; ++i)
+        {
+            //_canvasRaycaster.Add(CanvasList[i].GetComponent<GraphicRaycaster>());
+            //CanvasDict.Add(i, CanvasList[i]);
+            _canvasRaycaster.Add(_canvas.GetChild(i).GetComponent<GraphicRaycaster>());
+            CanvasDict.Add(i, _canvas.GetChild(i));
+        }
+
+        _cardEnlargePanel = Canvas(CanvasName.CardReward).Find("CardEnlargePanel");
+        _cardRewardContent = ContinueFindChildByName(Canvas(CanvasName.CardReward), "Content");
+        _itemEnlargePanel = Canvas(CanvasName.ItemReward).Find("ItemEnlargePanel");
+        _itemRewardContent = ContinueFindChildByName(Canvas(CanvasName.ItemReward), "Content");
+
+        _viewDeckContent = ContinueFindChildByName(Canvas(CanvasName.ViewDeck), "Content");
+        //for (int i = 0; i < ViewDeckContent.childCount; ++i)
+        //{
+        //    _deckUICards.Add(ViewDeckContent.GetChild(i).GetComponent<UICard>());
+        //}
+
+        PassiveTransform = ContinueFindChildByName(Canvas(CanvasName.InGame), "PassiveItem");
+        ActiveTransform = ContinueFindChildByName(Canvas(CanvasName.InGame), "ActiveItemButton");
+        PotionTransform = ContinueFindChildByName(Canvas(CanvasName.InGame), "PotionItem");
+
+        _shopPanel = Canvas(CanvasName.Shop).Find("ShopPanel");
+        _shopEnlargePanel = Canvas(CanvasName.Shop).Find("ShopEnlargePanel");
+
+        _topHealthText = ContinueFindChildByName(Canvas(CanvasName.InGame), "HealthText").GetComponent<TMP_Text>();
+        _topCoinText = ContinueFindChildByName(Canvas(CanvasName.InGame), "CoinText").GetComponent<TMP_Text>();
+
+        _holoValue = ContinueFindChildByName(Canvas(CanvasName.Battle), "HoloValueText").GetComponent<TMP_Text>();
+        _turnEndButtonText = ContinueFindChildByName(Canvas(CanvasName.Battle), "TurnText").GetComponent<TMP_Text>();
+        _drawCount = ContinueFindChildByName(Canvas(CanvasName.Battle), "DrawCountText").GetComponent<TMP_Text>();
+        _dummyCount = ContinueFindChildByName(Canvas(CanvasName.Battle), "DummyCountText").GetComponent<TMP_Text>();
+
+        Transform rewardBoxCanvas = Canvas(CanvasName.RewardBox);
+        _rewardBoxes = new Transform[rewardBoxCanvas.childCount];
+        for (int i = 0; i < _rewardBoxes.Length; ++i)
+        {
+            _rewardBoxes[i] = rewardBoxCanvas.GetChild(i);
+        }
+        for (int i = 0; i < _uiCards.Length; ++i)
+        {
+            _uiCards[i] = _cardRewardContent.GetChild(i).GetComponent<UICard>();
+        }
+        for (int i = 0; i < _uiItems.Length; ++i)
+        {
+            _uiItems[i] = _itemRewardContent.GetChild(i).GetComponent<UIItem>();
+        }
+    }
+
+    private void Start()
+    {
+        //_cardEnlargePanel = Canvas(CanvasName.CardReward).Find("CardEnlargePanel");
+        //_cardRewardContent = FindChildByName(Canvas(CanvasName.CardReward), "Content");
+        //_itemEnlargePanel = Canvas(CanvasName.ItemReward).Find("ItemEnlargePanel");
+        //_itemRewardContent = FindChildByName(Canvas(CanvasName.ItemReward), "Content");
+        //_shopPanel = Canvas(CanvasName.Shop).Find("ShopPanel");
+        //_shopEnlargePanel = Canvas(CanvasName.Shop).Find("ShopEnlargePanel");
+
+        //_topHealthText = FindChildByName(Canvas(CanvasName.InGame), "HealthText").GetComponent<TMP_Text>();
+        //_topCoinText = FindChildByName(Canvas(CanvasName.InGame), "CoinText").GetComponent<TMP_Text>();
+
+        //_holoValue = FindChildByName(Canvas(CanvasName.Battle), "HoloValueText").GetComponent<TMP_Text>();
+        //_turnEndButtonText = FindChildByName(Canvas(CanvasName.Battle), "TurnEndButton").GetComponent<TMP_Text>();
+
+        //Transform rewardBoxCanvas = Canvas(CanvasName.RewardBox);
+        //_rewardBoxes = new Transform[rewardBoxCanvas.childCount];
+        //for (int i = 0; i < _rewardBoxes.Length; ++i)
+        //{
+        //    _rewardBoxes[i] = rewardBoxCanvas.GetChild(i);
+        //}
+
+        //for (int i = 0; i < _uiCards.Length; ++i)
+        //{
+        //    _uiCards[i] = _cardRewardContent.GetChild(i).GetComponent<UICard>();
+        //}
+
+        SetActiveCanvas(CanvasName.Map, true);
+    }
+
+    public Transform ContinueFindChildByName(Transform parent, string name)
+    {
+        foreach (Transform child in parent)
+        {
+            if (child.name == name)
+                return child;
+
+            Transform found = ContinueFindChildByName(child, name);
+            if (found != null)
+                return found;
+        }
+        return null;
+    }
+
+    //public Transform FindChildByName(CanvasName canvasName, string name)
+    //{
+    //    return CanvasDict[(int)canvasName].Find(name);
+    //}
+
+
+    public void SetActiveCanvas(CanvasName canvasName, bool state, int idx = -1)
+    {
+        if (!state)
+        {
+            switch (canvasName)
+            {
+                case CanvasName.RewardBox:
+                    if (idx == -1) break;
+                    _rewardBoxes[idx].gameObject.SetActive(false);
+                    break;
+                case CanvasName.CardReward:
+                    _cardEnlargePanel.gameObject.SetActive(false);
+                    break;
+                case CanvasName.ItemReward:
+                    _itemEnlargePanel.gameObject.SetActive(false);
+                    break;
+                case CanvasName.Shop:
+                    _shopPanel.gameObject.SetActive(false);
+                    _shopEnlargePanel.gameObject.SetActive(false);
+                    break;
+            }
+
+            CanvasDict[(int)canvasName].gameObject.SetActive(false);
+        }
+        else
+        {
+            CanvasDict[(int)canvasName].gameObject.SetActive(true);
+
+            switch (canvasName)
+            {
+                case CanvasName.RewardBox:
+                    _rewardBoxes[idx].gameObject.SetActive(true);
+                    break;
+                case CanvasName.Map:
+                    if (Canvas(CanvasName.ViewDeck).gameObject.activeSelf)
+                    {
+                        SetActiveCanvas(CanvasName.ViewDeck, false);
+                        //InGameManager.Instance.Pause(false);      // UI가 막아서 뷰덱 중에는 맵 화면 클릭 불가
+                    }
+                    break;
+                case CanvasName.ViewDeck:
+                    if (Canvas(CanvasName.Map).gameObject.activeSelf)
+                        SetActiveCanvas(CanvasName.Map, false);
+                    break;
+            }
+        }
+
+    }
+
+    public Transform Canvas(CanvasName canvasName)
+    {
+        return CanvasDict[(int)canvasName];
+    }
+
+    public void SetCanvasRaycast(CanvasName canvasName, bool isOn)
+    {
+        _canvasRaycaster[(int)canvasName].enabled = isOn;
+    }
+
+    public void MoveMap()
+    {
+        SetActiveCanvas(CanvasName.CardReward, false);
+        SetActiveCanvas(CanvasName.ItemReward, false);
+        SetActiveCanvas(CanvasName.Shop, false);
+    }
+    public void ShowRewardCard(CardData[] reward)        // 해당 부분들 맵, 상점으로 다 이동시켜야 함.
+    {
+        for (int i = 0; i < reward.Length; ++i)
+        {
+            if (reward[i] == null) continue;        // 카드는 나중에 중복으로 떠도 되기 때문에 카드레어도에 따른 카드풀이 3~4장 이상이면 null이 뜰 가능성이 존재하지 않음.
+            _uiCards[i].Setup(/*InGameManager.Instance.FindCardData(reward[i])*/reward[i]);
+        }
+    }
+    public void ShowRewardItem(ItemData[] reward)
+    {
+        for (int i = 0; i < reward.Length; ++i)
+        {
+            if (reward[i] == null) continue;
+            _uiItems[i].Setup(reward[i]);
+        }
+    }
+    public void ChangeRewardCardCount(bool isOn)
+    {
+        _cardRewardContent.GetChild(3).gameObject.SetActive(isOn);
+    }
+    public void ChangeRewardItemCount(bool isOn)
+    {
+        _itemRewardContent.GetChild(3).gameObject.SetActive(isOn);
+    }
+
+    public void SetViewDeck(List<Card> deck)                // 풀링이지만, Release 개념이 아닌, 활성화 비활성화로 진행됨. Release는 인덱스로 넣는데, Get은 Release된 것 중에서 마지막에 넣었던 것을 꺼내오기 때문에 생긴 문제
+    {
+        _viewDeckContent.localPosition = new Vector3(_viewDeckContent.localPosition.x, 0);
+        if (deck.Count > _deckUICards.Count)
+        {
+            int deckUICardCount = _deckUICards.Count;
+            for (int i = 0; i < _lastViewDeckCount; ++i)
+            {
+                _deckUICards[i].Setup(deck[i].Data);
+            }
+            for (int i = _lastViewDeckCount; i < _deckUICards.Count; ++i)
+            {
+                _deckUICards[i].gameObject.SetActive(true);
+                _deckUICards[i].Setup(deck[i].Data);
+            }
+            for (int i = deckUICardCount; i < deck.Count; ++i)
+            {
+                _deckUICards.Add(Instantiate(_uiCard, _viewDeckContent));
+                _deckUICards[i].Setup(deck[i].Data);
+            }
+        }
+        else
+        {
+            if (deck.Count > _lastViewDeckCount)
+            {
+                for (int i = 0; i < _lastViewDeckCount; ++i)
+                {
+                    _deckUICards[i].Setup(deck[i].Data);
+                }
+                for (int i = _lastViewDeckCount; i < deck.Count; ++i)
+                {
+                    _deckUICards[i].gameObject.SetActive(true);
+                    _deckUICards[i].Setup(deck[i].Data);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < deck.Count; ++i)
+                {
+                    _deckUICards[i].Setup(deck[i].Data);
+                }
+                for (int i = deck.Count; i < _lastViewDeckCount; ++i)
+                {
+                    _deckUICards[i].gameObject.SetActive(false);
+                }
+            }
+        }
+        _lastViewDeckCount = deck.Count;
+        SetActiveCanvas(CanvasName.ViewDeck, true);
+        InGameManager.Instance.Pause(true);
+    }
+
+    //public void SetupGameUi(bool state)
+    //{
+    //    SetActiveCanvas(CanvasName.INGAME);
+    //}
+    //public void SetupBattleUi(bool state)
+    //{
+    //    SetActiveCanvas(CanvasName.BATTLE);
+    //}
+
+    //public void SetupSelectCardInBattleUi(bool state)
+    //{
+    //    foreach (GameObject gameObject in UI)
+    //    {
+    //        gameObject.SetActive(state);
+    //    }
+    //}
+
+    public void ChangeTurnButtonText(bool turn)
+    {
+        if (turn)
+            _turnEndButtonText.text = "Turn End";
+        else
+            _turnEndButtonText.text = "Enemy's Turn";
+    }
+    public void LookMap()
+    {
+        SetActiveCanvas(CanvasName.Map, !Canvas(CanvasName.Map).gameObject.activeSelf);
+    }
+
+    public void SetHolo(int curHolo, int maxHolo)
+    {
+        _holoValue.text = $"{curHolo} / {maxHolo}";
+    }
+
+    public void SetHealth(int curHp, int maxHp)
+    {
+        _topHealthText.text = $"{curHp} / {maxHp}";
+    }
+
+    public void SetCoin(int coin)
+    {
+        _topCoinText.text = coin.ToString();
+    }
+
+    public void SetDrawCount()
+    {
+        _drawCount.text = CardManager.Instance.DrawDeck.Count.ToString();
+    }
+
+    public void SetDummyCount()
+    {
+        _dummyCount.text = CardManager.Instance.CardDummy.Count.ToString();
+    }
+}
